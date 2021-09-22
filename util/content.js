a7d9840574cd46a515fc7d0db6dc66fbe1b3c184
hashika <m.g.hashikamaduranga@gmail.com>
2021-09-22 13:46:27 +0530

    add content util

---
+++ util/content.js
@@ -0,0 +1,10 @@
+exports.TimeRange = {
+    dayTime:{
+        time1:'10.00 am',
+        time2:'1.00 am',
+        time3:'3.00 am',
+        time4:'5.00 am',
+        time5:'6.00 am',
+        time6:'8.00 am',
+    }
+}
