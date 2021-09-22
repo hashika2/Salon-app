b30435d6813e21a27f0a951b4962cdf831465467
hashika <m.g.hashikamaduranga@gmail.com>
2021-09-22 11:12:52 +0530

    clean the code of list component

--- components/booking/BookList.js
+++ components/booking/BookList.js
@@ -3,18 +3,13 @@ import {
 	View,
 	Text,
 	TouchableOpacity,
-	StatusBar,
-	SafeAreaView,
 	StyleSheet,
-	TextInput,
-	Image,
 	ScrollView
 } from 'react-native';
-import { Card, ListItem, Button, Icon } from 'react-native-elements';
+import { Card, ListItem } from 'react-native-elements';
 import {getBookedData} from '../../service/StoreData';
 
 export default BookList = ({ navigation }) => {
-	const [ location, onChangeLocation ] = React.useState(null);
 	const lists = getBookedData();
 	return (
 		<View style={styles.container}>
@@ -22,7 +17,7 @@ export default BookList = ({ navigation }) => {
 				<ScrollView>
 					{lists.map((list, i) => {
 						return (
-							<TouchableOpacity key={i} onPress={() => navigation.navigate('Styling', list)}>
+							<TouchableOpacity key={i}>
 								<Card key={i}>
 									{/* <Card.Divider/> */}
 									<ListItem key={i} style={styles.cardContainer}>
@@ -31,10 +26,10 @@ export default BookList = ({ navigation }) => {
 												<Text style={styles.name1}>{list.name}</Text>
 												<Text style={styles.name2}>{list.date}</Text>
 												<Text style={{color:list.color}}>{list.validity}</Text>
-												{/* <Text style={styles.name3}>Staring ${list.price}</Text> */}
 											</View>
 										</View>
 									</ListItem>
+									<Text style={styles.name3}>${list.price}</Text>
 								</Card>
 							</TouchableOpacity>
 						);
@@ -58,7 +53,7 @@ const styles = StyleSheet.create({
 	},
 	cardContainer: {
 		width: 320,
-		height: 100
+		height: 70
 	},
 	user: {
 		flexDirection: 'row'
@@ -88,44 +83,20 @@ const styles = StyleSheet.create({
 	},
 	name3: {
 		marginTop: 5,
-		color: 'red'
-	},
-	input: {
-		height: 40,
-		width: 240,
-		marginTop: 60,
-		borderWidth: 1,
-		padding: 10
+		color: 'white',
+        textAlign:"center",
+        left:300,
+        bottom:90,
+        width:35,
+        backgroundColor:"green"
 	},
 	forgot_button: {
 		marginLeft: 10
-	},
-	loginBtn: {
-		width: '50%',
-		borderRadius: 25,
-		height: 50,
-		alignItems: 'center',
-		justifyContent: 'center',
-		marginTop: 40,
-		backgroundColor: 'green',
-		color: 'white'
-	},
-	loginText: {
-		fontSize: 20,
-		fontWeight: 'bold',
-		color: 'white'
-		// fontFamily:"fangsong"
 	},
 	homeTitle: {
 		fontSize: 25,
 		fontWeight: 'bold',
 		color: 'green'
 		// fontFamily:"fangsong"
-	},
-	accountText: {
-		paddingTop: 20
-	},
-	signUp: {
-		color: 'green'
 	}
 });
