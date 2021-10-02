e417866211655d9a92fff1fd51dc74b798a69b76
hashika <m.g.hashikamaduranga@gmail.com>
2021-10-02 16:36:03 +0530

    add heade changes

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View, Image,  TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native';
 // import Navigator from './routes/homeStack';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
@@ -17,66 +17,106 @@ import Appoinment from './components/booking/Appoinment';
 
 const Stack = createNativeStackNavigator();
 export default function App() {
-  return ( <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home"
-        headerMode="screen"
-        // screenOptions={{
-        //   header:({scene,navigation})=>(
-        //     <StackHeader scene={scene} navigation={navigation}/>
-        //   )
-        // }}
-      >
-        <Stack.Screen name="Home" component={Home}  options={{ headerShown: false }}/>
-        <Stack.Screen name="Login" component={Login}  options={{ headerShown: false }}/>
-        <Stack.Screen name="Register" component={Register}  options={{ headerShown: false }} />
-        <Stack.Screen name="Store" component={Store} style={styles.appbar} />
-        <Stack.Screen name="Styling" component={Styling}  options={{ headerShown: false }}/>
-        <Stack.Screen name="DateSelect" component={DateSelect}  options={{ title: 'Confirm Booking' }}/>
-        <Stack.Screen name="PickTime" component={PickTime} options={{ title: 'Confirm Booking' }}/>
-        <Stack.Screen name="BookList" component={BookList} options={{ title: 'Your Appoinment' }}/>
-        <Stack.Screen name="Appoinment" component={Appoinment}options={{ headerShown: false }}/>
-      </Stack.Navigator>
-    </NavigationContainer>)
+	return (
+		<NavigationContainer>
+			<Stack.Navigator
+				initialRouteName="Home"
+				headerMode="screen"
+				// screenOptions={{
+				//   header:({scene,navigation})=>(
+				//     <StackHeader scene={scene} navigation={navigation}/>
+				//   )
+				// }}
+			>
+				<Stack.Screen name="Home" component={Home} options={{ headerShown: false }} />
+				<Stack.Screen name="Login" component={Login} options={{ headerShown: false }} />
+				<Stack.Screen name="Register" component={Register} options={{ headerShown: false }} />
+				<Stack.Screen
+					name="Store"
+					component={Store}
+					style={styles.appbar}
+					options={{
+						title: 'Store',
+						headerStyle: { backgroundColor: 'green' },
+						headerTitleAlign: 'center',
+						headerTintColor: 'white'
+					}}
+				/>
+				<Stack.Screen name="Styling" component={Styling} options={{ headerShown: false }} />
+				<Stack.Screen
+					name="DateSelect"
+					component={DateSelect}
+					options={{
+						title: 'Confirm Booking',
+						headerStyle: { backgroundColor: 'green' },
+						headerTitleAlign: 'center',
+						headerTintColor: 'white'
+					}}
+				/>
+				<Stack.Screen
+					name="PickTime"
+					component={PickTime}
+					options={{
+						title: 'Confirm Booking',
+						headerStyle: { backgroundColor: 'green' },
+						headerTitleAlign: 'center',
+						headerTintColor: 'white'
+					}}
+				/>
+				<Stack.Screen
+					name="BookList"
+					component={BookList}
+					options={{
+						title: 'Your Appoinment',
+						headerStyle: { backgroundColor: 'green' },
+						headerTitleAlign: 'center',
+						headerTintColor: 'white'
+					}}
+				/>
+				<Stack.Screen name="Appoinment" component={Appoinment} options={{ headerShown: false }} />
+			</Stack.Navigator>
+		</NavigationContainer>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  appbar:{
-    backgroundColor:"green"
-  },  
-  homeTitle:{
-    fontSize: 25,
-    fontWeight: "bold",
-    color:"green",
-    // fontFamily:"fangsong"
-  },
-   image :{
-    width: 250,
-    height: 400,
-    marginBottom: 40,
-    marginTop: 100,
-    marginLeft:20,
-    marginRight: 20
-  },
-  loginBtn: {
-    width: "80%",
-    borderRadius: 25,
-    height: 50,
-    alignItems: "center",
-    justifyContent: "center",
-    marginTop: 40,
-    backgroundColor: "green",
-    color:"white"
-  },
-  loginText:{
-    fontSize: 20,
-    fontWeight: "bold",
-    color:"white",
-    // fontFamily:"fangsong"    
-  }
+	container: {
+		flex: 1,
+		backgroundColor: '#fff',
+		alignItems: 'center',
+		justifyContent: 'center'
+	},
+	appbar: {
+		backgroundColor: 'green'
+	},
+	homeTitle: {
+		fontSize: 25,
+		fontWeight: 'bold',
+		color: 'green'
+		// fontFamily:"fangsong"
+	},
+	image: {
+		width: 250,
+		height: 400,
+		marginBottom: 40,
+		marginTop: 100,
+		marginLeft: 20,
+		marginRight: 20
+	},
+	loginBtn: {
+		width: '80%',
+		borderRadius: 25,
+		height: 50,
+		alignItems: 'center',
+		justifyContent: 'center',
+		marginTop: 40,
+		backgroundColor: 'green',
+		color: 'white'
+	},
+	loginText: {
+		fontSize: 20,
+		fontWeight: 'bold',
+		color: 'white'
+		// fontFamily:"fangsong"
+	}
 });
