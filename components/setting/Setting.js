b103ff696ddf11a45f099e4be17dbfae37fec835
hashika <m.g.hashikamaduranga@gmail.com>
2021-10-02 16:21:39 +0530

    add new components

---
+++ components/setting/Setting.js
@@ -0,0 +1,10 @@
+import { View } from "react-native"
+import React, { useEffect } from 'react';
+
+export default Setting = () => {
+    return(
+        <View>
+            <Text>Settings</Text>
+        </View>
+    )
+}
---
+++ components/user/Profile.js
@@ -0,0 +1,10 @@
+import { View } from "react-native"
+import React, { useEffect } from 'react';
+
+export default Profile = () => {
+    return(
+        <View>
+            <Text>Profile</Text>
+        </View>
+    )
+}
